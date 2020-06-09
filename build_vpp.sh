d22f93e24759a1667730457c8abb0c16c95a5507
Artur Kraev <ravenox@gmail.com>
2020-06-09 22:33:28 +0300

    update to 20.05

--- build_vpp.sh
+++ build_vpp.sh
@@ -2,27 +2,21 @@
 
 set -e  # exit on the first command failure
 
-apt-get install -y python-cffi python-pycparser
+apt-get install -y python-cffi python-pycparser devscripts debhelper dpkg-dev dkms
 
-git clone https://github.com/garyachy/vppsb.git
-cd vppsb
-git checkout compile_problems
-cd -
+rm -r ./build
+git clone -b v20.05 https://github.com/RaveNoX/vppsb.git ./build/vppsb
+git clone -b v20.05 https://github.com/FDio/vpp.git ./build/vpp
 
-git clone https://github.com/FDio/vpp
-cd vpp
-git checkout stable/2001
-cd -
+ln -sf ../../../vppsb/netlink ./build/vpp/src/plugins/netlink
+ln -sf ../../../vppsb/router ./build/vpp/src/plugins/router
 
-ln -sf ../../../vppsb/netlink vpp/src/plugins/netlink
-ln -sf ../../../vppsb/router vpp/src/plugins/router
+#cp startup.conf vpp/src/vpp/conf/
+#cp init.txt vpp/src/vpp/conf/
 
-cp startup.conf vpp/src/vpp/conf/
-cp init.txt vpp/src/vpp/conf/
+#sed -i '$ a install(FILES conf/init.txt DESTINATION etc/vpp COMPONENT vpp)' vpp/src/vpp/CMakeLists.txt
 
-sed -i '$ a install(FILES conf/init.txt DESTINATION etc/vpp COMPONENT vpp)' vpp/src/vpp/CMakeLists.txt
-
-cd vpp
+cd ./build/vpp
 
 make install-ext-deps
 make install-dep
